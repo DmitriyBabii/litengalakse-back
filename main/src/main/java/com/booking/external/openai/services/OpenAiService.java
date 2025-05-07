0dd295703fedab8837a3b77bcf5045973c3970a3
DmitriyBabii <v.babiy75@gmail.com>
2025-05-07 23:02:11 +0300

    remove unused method

--- main/src/main/java/com/booking/external/openai/services/OpenAiService.java
+++ main/src/main/java/com/booking/external/openai/services/OpenAiService.java
@@ -5,7 +5,6 @@ import com.booking.external.openai.models.dtos.OpenAiMessage;
 import com.booking.external.openai.models.dtos.OpenAiRequest;
 import com.booking.external.openai.models.dtos.OpenAiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -35,19 +34,6 @@ public class OpenAiService {
         return restTemplate.postForObject(openAiProperties.getUrl(), openAiRequest, OpenAiResponse.class);
     }
 
-    public OpenAiRequest buildRequest() {
-        return OpenAiRequest.builder()
-                .model(openAiProperties.getModel())
-                .messages(List.of(
-                        OpenAiMessage.builder()
-                                .role("user")
-                                .content("Tell me something interesting")
-                                .build()
-                ))
-                .maxTokens(MAX_TOKENS)
-                .temperature(TEMPERATURE)
-                .build();
-    }
 
     public OpenAiRequest buildRequest(List<OpenAiMessage> messages) {
         return OpenAiRequest.builder()
