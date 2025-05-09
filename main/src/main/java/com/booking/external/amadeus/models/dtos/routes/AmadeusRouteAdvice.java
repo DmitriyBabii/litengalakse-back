7f0b9a432de33270395c09cf3f0fc526491d9980
DmitriyBabii <v.babiy75@gmail.com>
2025-05-09 22:54:41 +0300

    add Builder and amenities field to AmadeusRouteAdvice

--- main/src/main/java/com/booking/external/amadeus/models/dtos/routes/AmadeusRouteAdvice.java
+++ main/src/main/java/com/booking/external/amadeus/models/dtos/routes/AmadeusRouteAdvice.java
@@ -2,13 +2,16 @@ package com.booking.external.amadeus.models.dtos.routes;
 
 import com.booking.external.amadeus.models.dtos.hotel.AmadeusHotel;
 import com.booking.external.amadeus.models.dtos.hotel.AmadeusMeta;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record AmadeusRouteAdvice(
         String cityCode,
         String description,
         List<AmadeusHotel> data,
+        List<String> amenities,
         AmadeusMeta meta
 ) {
 }
