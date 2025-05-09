b10fba6c06615b73225f47c11e5b2b9e413ad2b9
DmitriyBabii <v.babiy75@gmail.com>
2025-05-09 22:55:26 +0300

    add amenities to response

--- main/src/main/java/com/booking/external/amadeus/models/dtos/routes/AmadeusAiRoute.java
+++ main/src/main/java/com/booking/external/amadeus/models/dtos/routes/AmadeusAiRoute.java
@@ -1,7 +1,10 @@
 package com.booking.external.amadeus.models.dtos.routes;
 
+import java.util.List;
+
 public record AmadeusAiRoute(
         String cityCode,
-        String description
+        String description,
+        List<String> amenities
 ) {
 }
