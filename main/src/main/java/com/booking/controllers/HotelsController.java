879b4817dceb0a89584309d9c65064189b9e1ac2
DmitriyBabii <v.babiy75@gmail.com>
2025-05-18 16:45:07 +0300

    remove unused methods and modify existed

--- main/src/main/java/com/booking/controllers/HotelsController.java
+++ main/src/main/java/com/booking/controllers/HotelsController.java
@@ -1,29 +1,23 @@
 package com.booking.controllers;
 
-import com.booking.external.amadeus.models.dtos.hotel.AmadeusHotelsResponse;
 import com.booking.external.amadeus.models.dtos.routes.AmadeusRouteAdvice;
 import com.booking.external.amadeus.services.AmadeusAiService;
-import com.booking.external.amadeus.services.AmadeusService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/hotels")
 @RequiredArgsConstructor
 public class HotelsController {
-    private final AmadeusService amadeusService;
     private final AmadeusAiService amadeusAiService;
 
     @PostMapping
-    public AmadeusHotelsResponse getHotelsByParams(@RequestBody Map<String, String> params) {
-        return amadeusService.findHotelsByCity(params);
-    }
-
-    @GetMapping
-    public List<AmadeusRouteAdvice> getHotelsWithAi(@RequestParam String prompt) {
+    public List<AmadeusRouteAdvice> getHotelsWithAi(@RequestBody String prompt) {
         return amadeusAiService.getAmadeusRouteAdvices(prompt);
     }
 }
